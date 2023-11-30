850a688a5a8d631a121010c20b2dc484f842bdc6
landrzejewski <landrzejewski.poczta@gmail.com>
2023-11-30 15:55:38 +0100

    x

--- src/test/java/pl/training/shop/integration/PaymentRestAdapterIntegrationTest.java
+++ src/test/java/pl/training/shop/integration/PaymentRestAdapterIntegrationTest.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,14 +18,16 @@ import java.util.Locale;
 
 import static org.hamcrest.core.Is.is;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.DEFINED_PORT;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static pl.training.shop.payments.PaymentFixtures.*;
 
+@ActiveProfiles("dev")
 @WithMockUser(roles = "ADMIN")
-@SpringBootTest(classes = Application.class, webEnvironment = DEFINED_PORT)
+@SpringBootTest(classes = Application.class, webEnvironment = RANDOM_PORT)
 @AutoConfigureMockMvc
 @ExtendWith(SpringExtension.class)
 class PaymentRestAdapterIntegrationTest {
